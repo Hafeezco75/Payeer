07ad61e41080ec775a63bd3a1949bdfcd6a08467
Hafeezco75 <hafeezco75@gmail.com>
2024-05-13 11:06:48 -0700

    Making changes to files

---
+++ gateOne/Day Seven/phoneBook/PhonebookApp.java
@@ -0,0 +1,25 @@
+public class PhonebookApp  {
+
+public static void main(String... args)  {
+
+String prompt = """
+1. Add Contact
+2. Remove Contact
+3. Find Contact by Phone Number
+4. Find Contact by Firstname
+5. Find Contact by lastname
+6. Edit Contact
+""";
+
+System.out.println(prompt);
+
+switch(userInput) {
+Case 1 ->
+Case 2 ->
+Case 3 ->
+Case 4 ->
+Case 5 ->
+Case 6 ->
+
+public static String[] addContact(
+
