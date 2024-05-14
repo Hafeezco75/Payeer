9003d3186a751b4c2cf60727ce858e9001a2f333
Hafeezco75 <hafeezco75@gmail.com>
2024-05-14 15:49:40 -0700

    Made some new changes to files

--- gateOne/Day Seven/menstrualWahala/MenstrualCycle.java
+++ gateOne/Day Seven/menstrualWahala/MenstrualCycle.java
@@ -12,16 +12,24 @@ public static void main(String... args)  {
 
 Scanner input = new Scanner(System.in);
 
-System.out.println("=============================================");
+System.out.println("=================================================");
 
 System.out.println("Welcome to Your EverInteractive Menstrual Cycle App");
 
-System.out.println("What is the Start date of your period");
+System.out.println("What is the Start date of your period,Enter in the format:(YYYY-MM-DD) ");
     String startDate = input.next();
 
-LocalDate now = LocalDate.of(startDate);
+int lengthToOvulation = 13;
 
-LocalDate after = now.plusMonths(1);
+int periodToFertility = 8;
+
+LocalDate localDate = LocalDate.parse(startDate);
+
+LocalDate after = localDate.plusDays(lengthToOvulation);
+
+LocalDate fertile = localDate.plusDays(periodToFertility);
+
+long noOfDaysBetween = ChronoUnit.DAYS.between(localDate, after);
 
 System.out.println("What is the average length of your menstrual cycle");
    int length = input.nextInt();
@@ -29,13 +37,15 @@ System.out.println("What is the average length of your menstrual cycle");
 
 System.out.println("if your menstrual cycle is " + length);
 
-//System.out.println("Your estimated fertile window is days);
+System.out.println("Your estimated fertile window is " + fertile + " - " + after);
 
-System.out.println("Your estimated ovulation date is  " + now);
+System.out.println("Your estimated ovulation date is " + after);
+
+System.out.println("Your safe period is " + );
 
 System.out.println("You are most likely to get pregnant if you have sex without birth control a few days before your ovulation date.");
 
-System.out.println("===============================================");
+System.out.println("===================================================");
 
  
 
